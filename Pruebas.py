66dd9b9fbb5670c255117d466bf2cf61e702394f
Kevin Cordero <38987651+Skryfall@users.noreply.github.com>
2018-05-06 18:47:44 -0600

    Version con movimiento por cuadrado de la matriz.

--- Pruebas.py
+++ Pruebas.py
@@ -1,13 +1,40 @@
-import pygame
-from pygame.locals import *
+import pygame as pygame
+
+cerrar = False
+
+filas = 25
+columnas = 40
+
+negro = (0, 0, 0)
+blanco = (255, 255, 255)
+
+pygame.init()
+
+pantalla = pygame.display.set_mode((800, 500))
+pygame.display.set_caption("PONG")
+pantalla.fill((10, 10, 10))
 
 class Juego:
-    def __init__(self):
-        self.matriz = hacer_matriz(25, 40)
+    def __init__(self, M):
+        self.M = M
+        for n in range(filas):
+            self.M.append([])
+            for m in range(columnas):
+                self.M[n].append(0)
+        for n in range(filas):
+            for m in range(columnas):
+                if n >= 7 and n <= 15 and m == 3:
+                    self.M[n][m] = 1
+                elif n >= 7 and n <= 15 and m == 36:
+                    self.M[n][m] = 1
+                elif n == 11 and m == 19:
+                    self.M[n][m] = 2
+                else:
+                    self.M[n][m] = 0
     def getMatriz(self):
-        return self.matriz
+        return self.M
     def setMatriz(self, nuevo):
-        self.matriz = nuevo
+        self.M = nuevo
 
 class Bola:
     def __init__(self):
@@ -54,32 +81,47 @@ class Barras:
         self.puntos = nuevo
     def setVelocidad(self, nuevo):
         self.velocidad = nuevo
+    def moverse(self, direccion):
+        M = juego.getMatriz()
+        i = 0
+        if direccion == "Arriba1":
+            while i != len(M[0]):
+                if M[i][3] == 1:
+                    M[i - 1][3] = 1
+                    M[i + 9][3] = 0
+                    juego.setMatriz(M)
+                    break
+                else:
+                    i += 1
 
-def hacer_matriz(filas, columnas):
-    M = []
-    subM = []
+juego = Juego([])
+barra1 = Barras(3, 0, 0, [2,3], 0, 4, 0)
+
+while not cerrar:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            cerrar = True
+        elif evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_UP:
+                barra1.moverse("Arriba1")
+
+    x = 0
+    y = 0
     n = 0
     m = 0
-    while n < filas:
-        while m < columnas:
-            subM.append(0)
+    while n != filas:
+        while m != columnas:
+            if juego.getMatriz()[n][m] == 1:
+                color = blanco
+            elif juego.getMatriz()[n][m] == 2:
+                color = blanco
+            else:
+                color = negro
+            pygame.draw.rect(pantalla, color, [x, y, 20, 20])
+            x += 20
             m += 1
-        m = 0
-        M.append(subM)
-        subM = []
+        x = 0
+        y += 20
         n += 1
-    return M
-
-
-pygame.init()
-pantalla = pygame.display.set_mode((1080, 720))
-pygame.display.set_caption("PONG")
-pantalla.fill((10, 10, 10))
-bola = Bola()
-while True:
-    bola.crearbola(pantalla)
-    for evento in pygame.event.get():
-        if evento.type == QUIT:
-            pygame.quit()
-            sys.exit()
+        m = 0
     pygame.display.update()
