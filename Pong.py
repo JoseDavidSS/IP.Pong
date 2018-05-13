1b0295cde2ff4057e8fb2cfad141e91ca6531cc8
JoseDavidSS <39009363+JoseDavidSS@users.noreply.github.com>
2018-05-13 15:42:31 -0600

    Se borran clases de barras y bola que no se usan en esta versión

--- Pong.py
+++ Pong.py
@@ -253,50 +253,5 @@ class Juego:
                                     justify="left")
         descripcion_controles.place(x=20,y=400)
 
-#Clase de Barras
-class Barras:
-    def __init__(self, largo, direccion_sobrebola, cpu, posicion, puntos, velocidad):
-        self.largo = largo
-        self.direccion_sobrebola = direccion_sobrebola
-        self.cpu = cpu
-        self.posicion = posicion
-        self.puntos = puntos
-        self.velocidad = velocidad
-    def getLargo(self):
-        return self.largo
-    def getDireccion(self):
-        return self.direccion_sobrebola
-    def getPosicion(self):
-        return self.posicion
-    def getPuntos(self):
-        return self.puntos
-    def getVelocidad(self):
-        return self.velocidad
-    def setPosicion(self, nuevo):
-        self.posicion = nuevo
-    def setPuntos(self, nuevo):
-        self.puntos = nuevo
-    def setVelocidad(self, nuevo):
-        self.velocidad = nuevo
-
-#Clase de Bola
-class Bola:
-    def __init__(self, posicion, velocidad, ultimo_golpe):
-        self.posicion = posicion
-        self.velocidad = velocidad
-        self.ultimo_golpe = ultimo_golpe
-    def getPosicion(self):
-        return self.posicion
-    def getVelocidad(self):
-        return self.velocidad
-    def getUltimo_golpe(self):
-        return self.ultimo_golpe
-    def setPosicion(self, nuevo):
-        self.posicion = nuevo
-    def setVelocidad(self, nuevo):
-        self.velocidad = nuevo
-    def setUltimo_Golpe(self, nuevo):
-        self.ultimo_golpe = nuevo
-
 Instancia_Juego = Juego("matriz")
 Instancia_Juego. menu_principal()
