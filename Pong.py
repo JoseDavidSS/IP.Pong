901cdd1b20c6c2e60f3ebdc9648703c81135baa8
Kevin Cordero <38987651+Skryfall@users.noreply.github.com>
2018-05-19 11:38:38 -0600

    Documentado

--- Pong.py
+++ Pong.py
@@ -5,39 +5,55 @@ import random
 import winsound
 from threading import Thread
 
-pantalla = []
+#Globales utilizadas por el programa
 
+#Variable principal del while de pygame
 cerrar = True
 
+#Cantidad de filas de la matriz
 filas = 25
+#Cantidad de columnas de la matriz
 columnas = 40
 
+#Indice el largo de la ventana de juego
 largo_pantalla = 1080
+#Indica el ancho de la ventana de juego
 ancho_pantalla = 720
 
+#Variables para calcular el tamaño de los pixeles de cada cuadro de la matriz
 largo_cuadro = largo_pantalla // columnas
 ancho_cuadro = ancho_pantalla // filas
 
+#Colores en RGB para usar en pygame
 negro = (0, 0, 0)
 blanco = (255, 255, 255)
 
+#Varables que controlan el nivel del juego y la cantidad de barras a utilizar por jugador
 nivel = 1
 barras = 1
 
+#Variables para manejar los FPS del juego
 reloj = pygame.time.Clock()
 Fps = 20
 
+#Variable para abrir la ventana de pygame
 comprobar = 0
 
+#Iniciación de pygame
 pygame.init()
 
 class Juego:
     def __init__(self, M):
+        #Se crea la matriz en el inicio de la clase, la matriz tiene solamente ceros
         self.M = M
         for n in range(filas):
             self.M.append([])
             for m in range(columnas):
                 self.M[n].append(0)
+    #Método de la clase para asignar las posiciones de los objetos en la matriz, varia dependiendo de los modos de juego elegidos
+    #Los datos de la matriz varían entre 0 (nada), los que inician con 1 son las barras, 11 (posición en donde rebota la bola hacia arriba), 12 (posicion en donde rebota la bola en línea recta), 13 (posicion en donde la bola rebota hacia abajo)
+    #31 (limite superior de la matriz, sirve para detener las barras y que la bola rebote), 32 (limite inferior de la matriz), 2 (la bola)
+    #41 (porteria del jugador 1), 42(porteria del jugador 2)
     def hacer_matriz(self):
         if barras == 1:
             if nivel == 1:
@@ -239,12 +255,17 @@ class Juego:
                             self.M[n][m] = 42
                         else:
                             self.M[n][m] = 0
+    #Métodos para obtener la matriz del juego y para sobreescribirla
     def getMatriz(self):
         return self.M
     def setMatriz(self, nuevo):
         self.M = nuevo
+    #Método para abrir la ventana del ganador de la partida
     def ganador(self):
+        #Es necesario volver a iniciar pygame porque se cerró al terminar la partida
         pygame.init()
+
+        #Creación de la ventana en tKinter y se le coloca un canvas
         ganador = Tk()
         ganador.title("Ganador")
         ganador.minsize(550, 300)
@@ -254,13 +275,16 @@ class Juego:
         canvasg.place(x=-2, y=-2)
         canvasg.pack()
 
+        #Función que permite volver al menú principal del juego
         def ganador_aux():
             ganador.destroy()
             juego.menu_principal()
 
+        #Función que se activa con un thread para reproducir el sonido de ganador
         def sonido_ganador():
             winsound.PlaySound("Ganador.wav", winsound.SND_ALIAS)
 
+        #Botones y labels de la ventana
         bt_listo = Button(canvasg,
                           text="Listo",
                           font=("Arial", 16),
@@ -284,11 +308,14 @@ class Juego:
                          fg="white")
             gano.place(x=145, y=30)
 
+        #Thread para iniciar el sonido
         t1 = Thread(target = sonido_ganador, args=())
         t1.start()
 
         ganador.mainloop()
+    #Método para abrir la ventana del menú principal
     def menu_principal(self):
+        #Se reinician las globales a sus valores originales y atributos
         global barras
         global Fps
         global comprobar
@@ -307,18 +334,17 @@ class Juego:
         barra1.setNiveles(0)
         barra2.setNiveles(0)
 
-        # Crear Ventana
+        #Creación de la ventana del menú principal y se loe coloca un canvas
         ventana = Tk()
         ventana.title("Menu Principal")
         ventana.minsize(1080, 720)
         ventana.resizable(width=NO, height=NO)
 
-        # Crear Canvas
         canvas = Canvas(ventana, width=1080, height=720, bg="black")
         canvas.place(x=-2, y=-2)
         canvas.pack()
 
-        # Funciones que llaman a los otros métodos de la clase con instancias
+        #Funciónes para llamar a los modos de juego o otras ventanas, y cerrar la del menú principal
         def jugador_CPU_aux():
             ventana.destroy()
             juego.jugador_CPU()
@@ -339,7 +365,7 @@ class Juego:
             ventana.withdraw()
             juego.ventana_instrucciones()
 
-        # Boton para iniciar partida de un jugador
+        #Botones para jugar
         un_jugador = Button(canvas,
                             text="Un jugador",
                             font=("Arial", 20),
@@ -364,7 +390,7 @@ class Juego:
                             command=jugador_doubles_aux)
         bt_doubles.place(x=425, y=450)
 
-        # Boton para ver ventana de descripciones de modos de juego
+        #Botón para acceder a la pantalla de los modos de juego
         bt_modos = Button(canvas,
                                text="Modos",
                                font=("Arial", 20),
@@ -373,7 +399,7 @@ class Juego:
                                command=ventana_modos_aux)
         bt_modos.place(x=425, y=580)
 
-        # Boton para ir a ventana de instrucciones
+        #Botón para acceder a la instrucciones
         bt_instrucciones = Button(canvas,
                                   text="Instrucciones",
                                   font=("Arial", 20),
@@ -382,7 +408,7 @@ class Juego:
                                   command=ventana_instrucciones_aux)
         bt_instrucciones.place(x=425, y=650)
 
-        # Labels
+        #Labels de la ventana
         titulo = Label(canvas,
                        text="PONG",
                        font=("Arial", 78),
@@ -392,6 +418,8 @@ class Juego:
 
         # Fin del loop
         ventana.mainloop()
+
+    #Funciones para iniciar los modos de juego
     def jugador_CPU(self):
         global cerrar
         barra2.setCPU(1)
@@ -407,23 +435,25 @@ class Juego:
         barra2.setCPU(0)
         juego.hacer_matriz()
         cerrar = False
+
+    #Método para abrir la ventana que explica los modos de juego
     def ventana_modos(self):
-        # Crear Ventana
+        #Creación de la ventana y su colocación del canvas
         ventana_modos = Tk()
         ventana_modos.title("Modos de Juego")
         ventana_modos.minsize(1080, 720)
         ventana_modos.resizable(width=NO, height=NO)
 
-        # Crear Canvas
         canvas_modos = Canvas(ventana_modos, width=1080, height=720, bg="black")
         canvas_modos.place(x=-2, y=-2)
         canvas_modos.pack()
 
+        #Función para volver al menú principal
         def volver():
             ventana_modos.destroy()
             juego.menu_principal()
 
-        # Botón para volver al menú principal
+        #Botón para volver al menú principal
         atras = Button(canvas_modos,
                        text="Volver al Menú Principal",
                        font=("Arial", 16),
@@ -431,7 +461,7 @@ class Juego:
                        command=volver)
         atras.place(x=820, y=25)
 
-        # Labels de descripciones de modos de juego
+        #Labels con descripciones de modos de juego
         descripcion_singles = Label(canvas_modos,
                                     text="Modo Singles es un un tipo de juego en el que participan\n dos jugadores, y cada jugador controla una paleta",
                                     font=("Arial", 20),
@@ -459,23 +489,25 @@ class Juego:
                                     bg="white",
                                     fg="black")
         lb_singles.place(x=410, y=125)
+
+    #Método para abrir la ventana de las instrucciones del juego
     def ventana_instrucciones(self):
-        # Crear Ventana
+        #Creación de la ventana y su canvas
         ventana_instrucciones = Tk()
         ventana_instrucciones.title("Intrucciones")
         ventana_instrucciones.minsize(1080, 720)
         ventana_instrucciones.resizable(width=NO, height=NO)
 
-        # Crear Canvas
         canvas_instrucciones = Canvas(ventana_instrucciones, width=1080, height=720, bg="black")
         canvas_instrucciones.place(x=-2, y=-2)
         canvas_instrucciones.pack()
 
+        #Función para volver al menú principal
         def volver():
             ventana_instrucciones.destroy()
             juego.menu_principal()
 
-        # Botón para volver al menú principal
+        #Botón para volver al menú principal
         atras = Button(canvas_instrucciones,
                        text="Volver al Menú Principal",
                        font=("Arial", 16),
@@ -483,7 +515,7 @@ class Juego:
                        command=volver)
         atras.place(x=820, y=25)
 
-        # Labels de descripcion de Juego y sus Controles
+        #Labels de descripcion del juego y sus controles
         descripcion_juego = Label(canvas_instrucciones,
                                   text="Descripción: \n \n"
                                        "Pong es un juego de deportes en dos dimensiones que simula un tenis de mesa. \n"
@@ -515,9 +547,11 @@ class Juego:
         descripcion_controles.place(x=20, y=400)
 
 class Bola:
+    #Atributos de la bola
     def __init__(self, velocidad, posicion):
         self.posicion = posicion
         self.velocidad = velocidad
+    #Métodos para obtener y sobreescribir la velocidad y posicion de la bola en la matriz
     def getPosicion(self):
         return self.posicion
     def getVelocidad(self):
@@ -526,6 +560,11 @@ class Bola:
         self.posicion = nuevo
     def setVelocidad(self, nuevo):
         self.velocidad = nuevo
+    #Método para el movimiento de la bola en la matriz, la velocidad de ella conforme avanzan niveles depende de los FPS del juego
+    #La bola para moverse utilizará su posición actual, y la sumará la velocidad en fila y columna para obtener el espacio al que se moverá
+    #En caso de que el espacio siguiente sea 0, avanzará
+    #En caso de que sea, 11, 12, 13, 31 o 32, la bola rebotará hacia cierta dirección
+    #En caso de que sea un 41 o un 42, se sumará un punto a la barra correspondiente
     def movimiento(self):
         global nivel
         global Fps
@@ -621,11 +660,14 @@ class Bola:
             bola.setPosicion(pos)
             bola.setVelocidad(vel)
             juego.setMatriz(M)
+
 class Barras:
+    #Atributos de la barras
     def __init__(self, CPU, puntos, niveles):
         self.CPU = CPU
         self.puntos = puntos
         self.niveles = niveles
+    #Métodos para obtener y sobreescribir los puntos, niveles ganados y el boolean para saber si es CPU de las barras
     def getPuntos(self):
         return self.puntos
     def getNiveles(self):
@@ -638,6 +680,10 @@ class Barras:
         self.niveles = nuevo
     def setCPU(self, nuevo):
         self.CPU = nuevo
+    #Método para reaizar los movimientos manuales (no cpu) de las barras
+    #Para moverse, se recibirá la tecla presionada por el usuario, (w arriba del jugador 1, s abajo del jugador 1, flecha arriba del jugador 2 y flecha abajo del jugador 2)
+    #Lo que hace es que empieza a leer la columna hasta encontrar el primer elemento de la barra en ella, a partir de ahí empezará a escirbir datos en la matriz con base al índice
+    #Varía dependiendo de las barras utilizadas y el nivel
     def moverse(self, direccion):
         M = juego.getMatriz()
         i = 0
@@ -1004,29 +1050,48 @@ class Barras:
                         else:
                             i += 1
 
-barra1 = Barras(1, 0, 0)
+#Se crean las instancias de las barras del jugador 1 y 2, el primer argumento es un boolean para indicar si es un cpu o no, el segundo son los puntos de la barra y el tercero son los niveles ganados
+barra1 = Barras(0, 0, 0)
 barra2 = Barras(0, 0, 0)
+
+#Se crea la instancia de la bola, el primer argumento será la velocidad de la fila y columna de la bola y el segundo será su posición inicial en la matriz
 bola = Bola([1,1],[11,19])
+
+#Se crea la instancia del juego, su argumento es la matriz a crear
 juego = Juego([])
+#Se abre el menú principal
 juego.menu_principal()
 
+#Función para obtener un valor entre 0 y 1, con tal de limitar el movimiento del cpu y que no sea imposible ganarle en el primer nivel
+#E: Función
+#S: Un número entre 0 y 1
+#R: Solo debe ser uno de esos dos números
 def hacer_random():
     if nivel == 1:
         ran = random.randint(0, 1)
         return ran
     else:
         return 0
+
+#While principal de pygame
 while not cerrar:
+    #Caso que solo se ejectua una vez por partida para abrir la ventana del juego
     if comprobar == 0:
         pantalla = pygame.display.set_mode((largo_pantalla, ancho_pantalla))
         pygame.display.set_caption("PONG")
         pantalla.fill(negro)
         comprobar = 1
+
+    #Método de pygame para controlar la velocidad del juego con los Fps
     reloj.tick(Fps)
+    #Se invoca el método de movimiento de la bola
     bola.movimiento()
+    #Variable que obtiene la tecla presionada por el usuario
     tecla = pygame.key.get_pressed()
+    #Variable que obtiene un valor al azar entre 0 y 1
     ran = hacer_random()
 
+    #Casos para detectar la tecla presionada y mover alguna de las barras o cerrar el juego
     if tecla[pygame.K_w] and juego.getMatriz()[0][2] == 31:
         barra1.moverse("Arriba1")
     if tecla[pygame.K_s] and juego.getMatriz()[24][2] == 32:
@@ -1039,6 +1104,9 @@ while not cerrar:
         if evento.type == pygame.QUIT:
             pygame.quit()
 
+    #Caso del movimiento de la barra 2, en caso de que sea un cpu
+    #Funciona de manera similar al método de movimiento de la barras manual, con la diferencia de que esta seguirá a la posicion de la fila de la bola
+    #Para que no sea imposible, se le pone una restricción en el nivel 1 que solo tiene permitido moverse cuando el número azar conseguido sea 0 y la bola se encuentre en cierto rango, para que no tenga problemas con la matriz
     if barra2.getCPU() == 1:
         vel = bola.getVelocidad()
         pos = bola.getPosicion()
@@ -1118,6 +1186,11 @@ while not cerrar:
                     else:
                         i += 1
 
+    #While que se encarga de dibujar la matriz en la pantalla, por cada vez que se presente un cambio en a matriz, esta función la reflejará de inmediato en la pantalla de juego
+    #La función lee columna por columna y luego pasa a la siguiente fila, si encuentra una barra o la bola, dibuja un cuadro blanco
+    #Si se encuentra en la columna central y en ciertas fila, dibujará la línea central
+    #Si alguna de las dos barras anota, dibujará un cuadro blanco en la parte superior de la pantalla
+    #Sino dibujará un cuadro negro
     x = 0
     y = 0
     n = 0
@@ -1159,6 +1232,8 @@ while not cerrar:
         m = 0
     pygame.display.update()
 
+    #Caso en caso de que el juego termine
     if barra1.getNiveles() == 2 or barra2.getNiveles() == 2:
+        cerrar = True
         pygame.quit()
         juego.ganador()
